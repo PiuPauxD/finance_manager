1095d306f00cbf81acfc5be605b85940c4d49986
PiuPauxD <olegich604@gmail.com>
2023-12-26 20:41:27 +0300

    fix ui on profile page

--- lib/ProfileTiles.dart
+++ lib/ProfileTiles.dart
@@ -13,30 +13,36 @@ class ProfileTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
       height: 80,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 15),
-          ),
-          Icon(
-            profileIcons,
-            size: 30,
-            color: Colors.black,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-          ),
-          Text(
-            settingName,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.grey.shade200,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 15),
             ),
-          ),
-        ],
+            Icon(
+              profileIcons,
+              size: 30,
+              color: Colors.black,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+            ),
+            Text(
+              settingName,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/Screens/ProfileScreen.dart
+++ lib/Screens/ProfileScreen.dart
@@ -1,6 +1,9 @@
 import 'package:finance_manager/ProfileTiles.dart';
 import 'package:finance_manager/Screens/BottomNavBar.dart';
+import 'package:finance_manager/widgets/drop_down.dart';
+import 'package:finance_manager/widgets/switch_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 List<String> language = [
   'Ru',
@@ -15,137 +18,148 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  bool isSwitched = false;
-
   String dropdownValue = language.first;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            //icon buttons
-            Expanded(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: ListView(
+                physics: const NeverScrollableScrollPhysics(),
                 children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) => const Navbar(),
-                        ),
-                      );
-                    },
-                    icon: const Icon(Icons.arrow_back_outlined),
-                    iconSize: 40,
-                    color: Colors.black,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.create_outlined),
-                    iconSize: 40,
-                    color: Colors.black,
-                  ),
-                ],
-              ),
-            ),
-            //image
-            Expanded(
-              flex: 2,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: Container(
-                  child: Image.asset(
-                    'img/profile1.jpg',
-                    // fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            //name
-            const Expanded(
-              flex: 1,
-              child: Text(
-                'Олег Сергеевич',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            //tiles
-            Expanded(
-              flex: 4,
-              child: Column(
-                children: [
-                  Flexible(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        //dark mode
-                        const ProfileTile(
-                          profileIcons: Icons.dark_mode_outlined,
-                          settingName: 'Ночная тема',
-                        ),
-                        Switch(
-                          value: isSwitched,
-                          onChanged: (value) {
-                            setState(() {
-                              isSwitched = value;
-                            });
-                          },
-                          activeTrackColor: Colors.grey,
-                          activeColor: Colors.yellow,
-                        ),
-                      ],
-                    ),
-                  ),
-                  //language
-
+                  //icon buttons
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const ProfileTile(
-                        profileIcons: Icons.language_outlined,
-                        settingName: 'Язык',
-                      ),
-                      DropdownMenu<String>(
-                        initialSelection: language.first,
-                        onSelected: (String? value) {
-                          setState(() {
-                            dropdownValue = value!;
-                          });
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) => const Navbar(),
+                            ),
+                          );
                         },
-                        dropdownMenuEntries:
-                            language.map<DropdownMenuEntry<String>>(
-                          (String value) {
-                            return DropdownMenuEntry<String>(
-                                value: value, label: value);
-                          },
-                        ).toList(),
+                        icon: const Icon(Icons.arrow_back_outlined),
+                        iconSize: 32,
+                        color: Colors.black,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.create_outlined),
+                        iconSize: 32,
+                        color: Colors.black,
                       ),
                     ],
                   ),
 
-                  //exit from app
-
-                  const Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ProfileTile(
-                          profileIcons: Icons.exit_to_app,
-                          settingName: 'Выход',
+                  //image
+                  Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        height: MediaQuery.of(context).size.height / 4,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
                         ),
-                      ],
+                        child: Image.asset(
+                          'img/profile1.jpg',
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  //name
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Center(
+                      child: Text(
+                        'Олег Сергеевич',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  //tiles
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 5, vertical: 60),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 2,
+                      child: ListView(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: [
+                          const Stack(
+                            children: [
+                              ProfileTile(
+                                profileIcons: Icons.bedtime_outlined,
+                                settingName: 'Ночная тема',
+                              ),
+                              Positioned(
+                                top: 15,
+                                right: 10,
+                                child: SwitchButton(),
+                              ),
+                            ],
+                          ),
+
+                          //language
+                          const Padding(
+                            padding: EdgeInsets.only(top: 5),
+                          ),
+                          const Stack(
+                            children: [
+                              ProfileTile(
+                                profileIcons: Icons.language_outlined,
+                                settingName: 'Язык',
+                              ),
+                              Positioned(
+                                top: 25,
+                                right: 10,
+                                child: DropDown(),
+                              ),
+                            ],
+                          ),
+
+                          //exit from app
+                          const Padding(
+                            padding: EdgeInsets.only(top: 5),
+                          ),
+                          Stack(
+                            children: [
+                              const ProfileTile(
+                                profileIcons: Icons.exit_to_app_outlined,
+                                settingName: 'Выход',
+                              ),
+                              Positioned(
+                                top: 15,
+                                right: 10,
+                                child: IconButton(
+                                  onPressed: () {
+                                    SystemNavigator.pop();
+                                  },
+                                  icon: const Icon(
+                                      Icons.arrow_forward_ios_outlined),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
